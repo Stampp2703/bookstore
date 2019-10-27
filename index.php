52bcb33803ab134704c9bf606deac3943fcea9ae
stamp <614259010@webmail.npru.ac.th>
2019-10-28 01:35:27 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<!----css--->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<!----css--->
+
+<!----css ระยะห่างของการ์ด--->
+<style>
+.card{ margin:30px}
+</style>
+<!----css ระยะห่างของการ์ด--->
+</head>
+
+<body>
+
+    <!----container--->
+    <div class="container-fluid">
+
+
+    <!----bar---->
+    <nav class="navbar navbar-expand-lg navbar-white bg-dark"><!-----bg bar----->
+    <img src="image/logobookstore.png" width="221" height="87" longdesc="image/logobookstore.png" /> <!----logo--->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"   aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+<!------------------------menu----------------------->
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      
+      <li class="nav-item active">
+        <a class="nav-link" href="#" style="color:#F90">New Arrival<span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color:#F90">New Update</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color:#F90">ชำระเงิน</a>
+      </li>
+      
+    </ul>
+    
+    <!----search------>
+tton    </form>
+  </div>
+  
+  <!------------------------end menu----------------------->
+</nav>
+<!----end bar---->
+
+
+<!---------------carusel------------------>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+ 
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-interval="5000"><!---------------carusel auto------------------>
+      <img src="image/niyayjamsai001.jpg" class="d-block w-100" alt="...">
+    </div>
+    
+   <div class="carousel-item">
+      <img src="image/niyayjamsai002.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="image/niyayjamsai003.jpg" class="d-block w-100" alt="...">
+    </div>
+    
+      
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!---------------end carusel------------------>
+
+
+
+
+<!---------------start row------------------>
+
+<div class="row">
+
+    
+   <div class="col-3"> <!---colum---->
+   <!----card---->
+    <div class="card" style="width: 18rem;">
+  <img src="image/niyay01.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">ปราบพยศหัวใจนายสุดฮอต!</h5>
+    
+    
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-dark" data-toggle="modal" data-target="#niyay01">
+  Add to cart
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="niyay01" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">ปราบพยศหัวใจนายสุดฮอต!</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img src="image/niyay01.jpg" class="card-img-top" alt="...">
+        250 bath
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Buy</button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+    
+    
+  </div>
+  </div>
+  <!----card---->
+   </div><!---end colum---->
+   
+   
+   <div class="col-3"> <!---colum---->
+   <!----card---->
+    <div class="card" style="width: 18rem;">
+  <img src="image/niyay02.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">หลงรักเธอเผลอให้หมดใจ</h5>
+    
+    
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-dark" data-toggle="modal" data-target="#niyay02">
+  Add to cart
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="niyay02" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">หลงรักเธอเผลอให้หมดใจ</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       <img src="image/niyay02.jpg" class="card-img-top" alt="...">
+       250 bath
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Buy</button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+    
+    
+  </div>
+  </div>
+  <!----card---->
+   </div><!---end colum---->
+   
+   
+   <div class="col-3"> <!---colum---->
+   <!----card---->
+    <div class="card" style="width: 18rem;">
+  <img src="image/niyay03.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">รักหวานฉ่ำวันฝนพร่ำ</h5>
+    
+    
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-dark" data-toggle="modal" data-target="#niyay03">
+  Add to cart
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="niyay03" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">รักหวานฉ่ำวันฝนพร่ำ</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div> 
+      <div class="modal-body">
+        <img src="image/niyay03.jpg" class="card-img-top" alt="...">
+        250 bath
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Buy</button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+    
+    
+  </div>
+  </div>
+  <!----card---->
+   </div><!---end colum---->
+   
+   
+   
+   <div class="col-3"> <!---colum---->
+   <!----card---->
+    <div class="card" style="width: 18rem;">
+  <img src="image/niyay04.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">ภารกิจรักจากต่างดาว</h5>
+    
+    
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-dark" data-toggle="modal" data-target="#niyay04">
+  Add to cart
+  </button>
+
+<!-- Modal -->
+<div class="modal fade" id="niyay04" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">ภารกิจรักจากต่างดาว</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img src="image/niyay04.jpg" class="card-img-top" alt="...">
+         250 bath
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Buy</button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+    
+    
+  </div>
+  </div>
+  <!----card---->
+   </div><!---end colum---->
+   
+
+</div>
+
+<!---------------end row------------------>
+
+       
+
+
+
+</div>
+<!----end container--->
+
+
+
+
+<!----JS--->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!----Js--->
+
+
+</body>
+</html>
